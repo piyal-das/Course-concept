405d76f9d00a64f7c1eb5b0dd8fed8c2f9813aee
piyal-das <66130612+piyal-das@users.noreply.github.com>
2020-08-28 09:21:25 +0600

    Course-list-created

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+
+
+
+import Course from './Component/Course/Course';
+
+
+
+function App() {
+  return (
+        <div>
+          
+          <Course></Course>
+         
+      
+        </div>
+  );
+}
+
+export default App;
